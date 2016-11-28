e889fc0ce7b49429744faa02bf3475091cfbf645
Jadon Wagstaff <jadonw@gmail.com>
2016-11-28 11:54:20 -0700

    update

--- public/js/main.js
+++ public/js/main.js
@@ -16,7 +16,35 @@ function loadData() {
         var chart = new Chart(data);
         var graphs = new Graphs(data);
         var list = new List(map, chart, graphs, data);
+	var stat = new Stat(data);
     });
 
+}
+
+//loads search function
+//using modified code from http://www.w3schools.com/howto/howto_js_filter_lists.asp
+function searchFunction() {
+    // Declare variables
+    var input, filter, ul, li, i;
+    input = document.getElementById('search');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("countryList");
+	li = ul.getElementsByTagName('li');
+
+	//console.log(li[0].className);
+	
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+		if(li[i].className == "list"){
+			if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+				li[i].style.display = "";
+			} else {
+				li[i].style.display = "none";
+			}
+		}else {
+			li[i].style.display = "none";
+		}
+        
+    }
 }
 
